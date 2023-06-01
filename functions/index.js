41b5b3b2a573555f0fa55d80c9e4472e2e3a87cd
Po-Cheng Yeh <blp100x@hotmail.com>
2023-06-01 16:09:05 +0800

    fix: change index.js port setting

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+const functions = require("firebase-functions");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -144,6 +146,16 @@ app.get("/:cutstomeListName", (req,res)=>{
 
 });
 
-app.listen(port, function() {
-  console.log("Server started on port " + port);
-});
+// app.listen(port, function() {
+//   console.log("Server started on port " + port);
+// });
+//don't listen the port, if you want to use firebase
+exports.app = functions.https.onRequest(app);
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
